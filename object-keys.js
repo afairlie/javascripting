d4b7ddcb97c6117b8000252b413e03b99468b537
afairlie <ariane.fairlie@gmail.com>
2020-02-14 19:32:17 +0000

    create an object, print the value of a certain key, print the names of the keys in an object

---
+++ object-keys.js
@@ -0,0 +1,9 @@
+const car = {
+  make: "Honda",
+  model: "Accord",
+  year: 2020
+}
+
+const keys = Object.keys(car) // this creates a variable called 'keys' that holds an array that lists the keys (as in the key: value pair in an object) of the car object by using the Object.keys() prototype method
+
+console.log(keys) // prints the values of the key variable
